967cda7f3622d3548e1ec74cc4e9e82338be9421
varut1976 <varut1976@gmail.com>
2022-02-11 12:32:26 +0200

    initial

---
+++ js/index.js
@@ -0,0 +1,15 @@
+// Наименование товара
+// Количество товара
+// Категория товара
+// Цена товара
+
+'use strict';
+
+const nameOfProduct = 'Bike';
+let numberOfProduct = 6;
+const productCategory = 'vehicle';
+let priceOfProduct = 30;
+
+const costProduct = numberOfProduct * priceOfProduct + '$';
+
+console.log(costProduct);
