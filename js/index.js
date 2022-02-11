de3e35933a8874b0ca8679e3fbe8c1126ee17e44
varut1976 <varut1976@gmail.com>
2022-02-11 19:53:34 +0200

    добавлены скобки для ограждения блоко

--- js/index.js
+++ js/index.js
@@ -1,23 +1,22 @@
-// Наименование товара
-// Количество товара
-// Категория товара
-// Цена товара
-
 'use strict';
 
-const nameOfProduct = 'Bike';
-let numberOfProduct = 6;
-const productCategory = 'vehicle';
-let priceOfProduct = 30;
+{
+    const nameOfProduct = 'Bike';
+    let numberOfProduct = 6;
+    const productCategory = 'vehicle';
+    let priceOfProduct = 30;
 
-const costProduct = numberOfProduct * priceOfProduct + '$';
+    const costProduct = numberOfProduct * priceOfProduct + '$';
+}
 
-const item = 'TV';
-const count = 4;
-const category = 'video';
-const price = 700;
+{
+    const item = 'TV';
+    const count = 4;
+    const category = 'video';
+    const price = 700;
 
-const cost = count * price + '$';
+    const cost = count * price + '$';
 
-console.log(item);
-console.log(`Общая сумма товара: ${cost}`);
+    console.log(item);
+    console.log(`Общая сумма товара: ${cost}`);
+}
